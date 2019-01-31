31bdc2eb9db2c882b73346e2f3705dcd13b07c6e
Alexi Most <alexibmost@gmail.com>
2019-01-30 17:59:21 -0800

    proj files

---
+++ ManagingErrors/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingErrors
+{
+    class Program
+    {
+
+        static string[] badValues = { "bad", "test" };
+        static string replacement = "*";
+        static char command = '-';
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to ChatBot\nPress e to exit");
+        }
+        static string TakeInput()
+        {
+            Console.WriteLine("Say something:");
+            string s = Console.ReadLine();
+            if (!CheckCommand(s))
+            {
+                s = Filter(s);
+                Console.WriteLine("You said:\n" + s);
+            }           
+            return s;
+        }
+        static string Filter(string s)
+        {
+            foreach(string value in badValues)
+            {
+                int replacementCount = value.Length;
+                // replace bad val with same number of characters
+                s = s.Replace(value, String.Concat(Enumerable.Repeat(replacement, value.Length)));
+            }
+            return s;
+        }
+
+        // returns if string was a command
+        static bool CheckCommand(string s)
+        {
+            if (s[0] != command) return false;
+
+            // check for commands
+            if(s == "-t")
+            {
+                Console.WriteLine("you want to test something");
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            // welcome
+            // take inout
+            // check commands
+            // check filter
+            // print input/response
+            Welcome();
+            string input = "";
+            while(input != "e")
+            {
+                input = TakeInput();
+            }
+        }
+       
+    }
+}
